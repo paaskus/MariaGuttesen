c000d485a90b834b2c593208ec5284356b2ac5d4
paaskus <bardurrunason@gmail.com>
2015-09-27 16:34:46 +0200

    title names are now solely lowecase

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var titles = [
-    {name: "Bass Player", an: false, path: "COMP/BassPlayerCOMP.mp4"},
-    {name: "Drummer", an: false, path: "COMP/DrummerCOMP.mp4"},
-    {name: "Improviser", an: true, path: "COMP/ImproviserCOMP.mp4"},
-    {name: "Keyboard Player", an: false, path: ["COMP/PianoPlayerCOMP.mp4", "COMP/PianoPlayer2COMP.mp4"]},
-    {name: "Producer", an: false, path: ["COMP/ProducerCOMP.mp4", "COMP/Producer2COMP.mp4"]},
-    {name: "Singer", an: false, path: "COMP/SingerCOMP.mp4"}
+    {name: "bass player", an: false, path: "COMP/BassPlayerCOMP.mp4"},
+    {name: "drummer", an: false, path: "COMP/DrummerCOMP.mp4"},
+    {name: "improviser", an: true, path: "COMP/ImproviserCOMP.mp4"},
+    {name: "keyboard Player", an: false, path: ["COMP/PianoPlayerCOMP.mp4", "COMP/PianoPlayer2COMP.mp4"]},
+    {name: "producer", an: false, path: ["COMP/ProducerCOMP.mp4", "COMP/Producer2COMP.mp4"]},
+    {name: "singer", an: false, path: "COMP/SingerCOMP.mp4"}
 ];
 
 var previousTitle = Math.floor(Math.random() * titles.length);
