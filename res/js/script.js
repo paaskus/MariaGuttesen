8c481703381c3652fe971f60ac7503fbb0c6e670
paaskus <bardurrunason@gmail.com>
2015-12-11 04:14:48 +0100

    Changed folder structure

---
+++ res/js/script.js
@@ -0,0 +1,111 @@
+var videosData = [
+    {title: "bass player", an: false, path: "vid/BassPlayerMORECOMP.mp4"},
+    {title: "drummer", an: false, path: "vid/DrummerMORECOMP.mp4"},
+    {title: "improviser", an: true, path: "vid/ImproviserMORECOMP.mp4"},
+    {title: "keyboard player", an: false, path: ["vid/PianoPlayerMORECOMP.mp4", "vid/PianoPlayer2MORECOMP.mp4"]},
+    {title: "producer", an: false, path: ["vid/ProducerMORECOMP.mp4", "vid/Producer2MORECOMP.mp4"]},
+    {title: "singer", an: false, path: "vid/SingerMORECOMP.mp4"}
+];
+
+var imagesData = [
+    {title: "bass player", an: false, path: "img/backgrounds/bass.png"},
+    {title: "drummer", an: false, path: "img/backgrounds/drummer.png"},
+    {title: "improviser", an: true, path: "img/backgrounds/improviser.png"},
+    {title: "keyboard player", an: false, path: ["img/backgrounds/piano.png", "img/backgrounds/piano2.png", "img/backgrounds/piano3.png"]},
+    {title: "producer", an: false, path: ["img/backgrounds/producer.png", "img/backgrounds/producer2.png"]},
+    {title: "singer", an: false, path: ["img/backgrounds/singer.png", "img/backgrounds/singer2.png"]}
+];
+
+// main function
+$(document).ready(function() {
+    handleBackgroundBasedOnDevice();
+});
+
+// Inserts image-backgrounds
+var imageBackgroundHandler = {
+    previousBackground: (Math.floor(Math.random() * imagesData.length)),
+    backgrounds: imagesData,
+    insertBackground: function(sourceOfImage) {
+        // inserts a new video element
+        var imageElement = $(document.createElement('img'))
+            .attr({"id": "currentBackground", "class": "background", "src": sourceOfImage});
+        $("#body").append(imageElement);
+    },
+    delay: 500
+};
+
+// Inserts video-backgrounds
+var videoBackgroundHandler = {
+    previousBackground: (Math.floor(Math.random() * videosData.length)),
+    backgrounds: videosData,
+    insertBackground: function(sourceOfVid) {
+        // inserts a new video element
+        var videoElement = $(document.createElement('video')).attr({"id": "currentBackground", "class": "background", "preload": "auto"});
+        videoElement.append($(document.createElement('source')).attr({"src": sourceOfVid, "type": "video/mp4"}));
+        $("#body").append(videoElement);
+        videoElement[0].play(); // start the video
+    },
+    delay: 1100
+};
+
+var backgroundHandler;
+
+// determines whether the browser supports video-backgrounds
+function handleBackgroundBasedOnDevice() {
+    var autoplay = detect_autoplay();
+    // if browser supports autoplay (dependant on detect_autoplay.js)
+    backgroundHandler = (autoplay) ? videoBackgroundHandler : imageBackgroundHandler;
+    updateBackground(backgroundHandler.previousBackground);
+    setInterval(function() {
+        updateBackground(backgroundHandler.previousBackground);
+    }, 14500);
+}
+
+// removes the 'backgroundToRemove' element
+function removePreviousBackground() {
+    $("#backgroundToRemove").remove();
+}
+
+// idOfPreviousBackground -> int
+function updateBackground(idOfPreviousBackground) {
+    // Find index of next video randomly
+    var num = getRandomNumber(backgroundHandler.backgrounds.length, backgroundHandler.previousBackground);
+    backgroundHandler.previousBackground = num; // preparation; update previousBackground
+    var nextBackgroundObject = backgroundHandler.backgrounds[num];
+    var source = nextBackgroundObject.path;
+    if (typeof source === "object") {
+        source = source[getRandomNumber(source.length)];
+    }
+
+    // prepare for deletion of existing background element
+    var backgroundToRemove = $("#currentBackground");
+    backgroundToRemove.attr("id", "backgroundToRemove");
+    backgroundToRemove.css("z-index", "-90"); // Show in front of nextBackground
+
+    // insert next background; fadeout old
+    backgroundHandler.insertBackground(source);
+    backgroundToRemove.fadeOut(backgroundHandler.delay);
+    $("#title").text(nextBackgroundObject.title);
+
+    // update an/a and the title
+    $("#as").html("as " + (nextBackgroundObject.an ? "an" : "a"));
+
+    // remove old video element after fadeout
+    setTimeout(function() {
+    	removePreviousBackground();
+    }, backgroundHandler.delay);
+}
+
+/**
+ * returns a random num randomNumber between 0 and n where randomNumber != doNotInclude
+ * @param  {Int} n            [description]
+ * @param  {Int} doNotInclude [description]
+ * @return {Int} randomNumber [description]
+ */
+function getRandomNumber(n, doNotInclude) {
+    var randomNumber = Math.floor(Math.random() * n);
+    while (randomNumber === doNotInclude) {
+        randomNumber = Math.floor(Math.random() * n);
+    }
+    return randomNumber;
+}
